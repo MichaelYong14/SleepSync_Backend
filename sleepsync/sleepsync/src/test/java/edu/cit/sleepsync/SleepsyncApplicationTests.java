90d05d9c7de2f653b70f9254b2a1cc2d965a2aee
MichaelYong14 <michaelanthony.yong@cit.edu>
2025-02-10 08:55:32 +0800

    Add files via upload

---
+++ sleepsync/sleepsync/src/test/java/edu/cit/sleepsync/SleepsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.sleepsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleepsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
